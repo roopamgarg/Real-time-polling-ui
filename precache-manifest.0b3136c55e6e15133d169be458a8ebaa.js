31f041b4c32e761867434ce4121d3766976d186a
Roopam Garg <roopamg777@gmail.com>
2021-03-23 01:15:28 +0530

    Updates

---
+++ precache-manifest.0b3136c55e6e15133d169be458a8ebaa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e899a80da245bec79bb35185a68ed542",
+    "url": "/Real-time-polling-ui/index.html"
+  },
+  {
+    "revision": "86dc778f8f4f5c77197c",
+    "url": "/Real-time-polling-ui/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "009de5a94ed76f17bef5",
+    "url": "/Real-time-polling-ui/static/css/main.73d094ab.chunk.css"
+  },
+  {
+    "revision": "86dc778f8f4f5c77197c",
+    "url": "/Real-time-polling-ui/static/js/2.395da9cb.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/Real-time-polling-ui/static/js/2.395da9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009de5a94ed76f17bef5",
+    "url": "/Real-time-polling-ui/static/js/main.a9017c03.chunk.js"
+  },
+  {
+    "revision": "025ba1911a22fd1d489a",
+    "url": "/Real-time-polling-ui/static/js/runtime-main.42aacb47.js"
+  },
+  {
+    "revision": "b98ba7d17b6a37d0a99e8147a97fb1f1",
+    "url": "/Real-time-polling-ui/static/media/pie-chart.b98ba7d1.svg"
+  },
+  {
+    "revision": "57a833b4127710763fa129314d37108a",
+    "url": "/Real-time-polling-ui/static/media/poll_creator.57a833b4.svg"
+  },
+  {
+    "revision": "5268775a5d245415bb5f79f8af6899e8",
+    "url": "/Real-time-polling-ui/static/media/poll_hero.5268775a.svg"
+  },
+  {
+    "revision": "c1b2757061ca23126361317ad99c490a",
+    "url": "/Real-time-polling-ui/static/media/real_time.c1b27570.svg"
+  }
+]);
