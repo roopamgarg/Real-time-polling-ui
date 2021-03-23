82364b330ac5019f9f397b70d8c271a09452ddcb
Roopam Garg <roopamg777@gmail.com>
2021-03-23 10:43:52 +0530

    added server url

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:4000/";
-export const SOCKET_URL = "http://localhost:4000/";
+export const API_URL = "http://voteforbest.herokuapp.com/";
+export const SOCKET_URL = "http://voteforbest.herokuapp.com/";
